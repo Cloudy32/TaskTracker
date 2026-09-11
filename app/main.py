6056f4b160779b7c7b7bb6f3b8c69260722ac08d
Cloudy32 <r.bondar29@gmail.com>
2026-09-11 16:57:05 +0300

    add alembic migrations

--- app/main.py
+++ app/main.py
@@ -1,10 +1,5 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from app.dataBase.base import Base
-from app.dataBase.session import engine
 
 from app.core.config import get_settings
 
@@ -12,14 +7,9 @@ from app.api.routers.task import task_router
 from app.api.routers.category import category_router
 
 
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    Base.metadata.create_all(bind=engine)
-    yield
-
 settings = get_settings()
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(task_router)
 app.include_router(category_router)
 
